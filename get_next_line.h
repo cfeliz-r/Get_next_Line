6d2f55d8d732d4a9448d5102e696cfe4f5eb8ed1
CarlosDavidFelizRamirez <carlos-r40@hotmail.com>
2024-04-25 16:43:13 +0200

    Principios de Get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfeliz-r <cfeliz-r@student.your42network.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 15:34:20 by cfeliz-r          #+#    #+#             */
+/*   Updated: 2024/04/25 16:41:26 by cfeliz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+char	*get_next_line(int fd);
+
+#endif
